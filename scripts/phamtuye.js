ae38b0e870595b2815490fe9d6bfcd1fa328e86c
Mai Pham <mai291294@gmail.com>
2023-02-16 19:00:21 -0500

    updated

--- scripts/phamtuye.js
+++ scripts/phamtuye.js
@@ -28,6 +28,7 @@ async function startUp() {
             console.log(student);
             loadHeader(student);
             loadFooter(cite);
+            loadAuthors(authorArray);
         })
     
     function loadInfo(dataJSON) {
@@ -37,18 +38,40 @@ async function startUp() {
                     studentInfo.UserName, studentInfo.Program);
 
         let authors = dataJSON.Authors;
-        for(let author of authors) {
+        for(let [index, author] of authors) {
             let au = new Author(author.authorName, author.picture);
-            console.log(au);
-            console.log(authorArray);
             authorArray.push(au);
+
+            //create div container for each author
+            let authorContainer = document.createElement("div");
+            authorContainer.classList.add("author-container");
+            
+            //create author thumbnail image
+            let authorImg = document.createElement("img");
+            authorImg.src = author.picture;
+            authorImg.classList.add("author-thumbnail-img");
+
+            //create anchor tag with author's name
+            let authorNameLink = document.createElement("a");
+            authorNameLink.href = "otherPages/author-info.html";
+            authorNameLink.innerText = author.authorName;
+            authorNameLink.onclick = `selectAuthor(${index}, ${authorArray})`;
+
+            let linebreak = document.createElement("br");
+
+            authorContainer.appendChild(authorImg);
+            authorContainer.appendChild(linebreak);
+            authorContainer.appendChild(authorNameLink);
+
+            document.querySelector(".author-list").appendChild(authorContainer);
+
         }
     
         cite = dataJSON.Reference;
     }
 
     function loadHeader(student) {
-        document.querySelector('header').innerHTML = 
+        document.querySelector('#header-text').innerHTML = 
         `
             <h4>Assignment #2/ Winter 2023<br>
             My Name: ${student.FullName}/ Student ID: ${student.StudentID}/ 
@@ -61,6 +84,11 @@ async function startUp() {
         `
             <b>${cite}</b>
         `
+    }
+
+    function selectAuthor(index, authorArray) {
+        localStorage.setItem("rowID", index);
+        localStorage.setItem("authorArray", JSON.stringify(authorArray));
     }
 }
 
